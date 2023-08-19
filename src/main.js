d4ffa36f18b92949ebe9ee78ccd4bd9c615721a3
Fabricyo Ribeiro <fabricyosilva600@gmail.com>
2023-08-19 14:57:06 -0300

    deply

--- src/main.js
+++ src/main.js
@@ -2,7 +2,41 @@ window.onload = function () {
   Particles.init({
     selector: '.background',
     color: ['#565656'],
-    connectParticles: true
+    connectParticles: true,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        options: {
+          maxParticles: 80,
+          color: '#565656',
+          connectParticles: true
+        }
+      },
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 50,
+          color: '#565656',
+          connectParticles: true
+        }
+      },
+      {
+        breakpoint: 425,
+        options: {
+          maxParticles: 30,
+          connectParticles: true
+        }
+      },
+      {
+        breakpoint: 320,
+        options: {
+          maxParticles: 0
+
+          // disables particles.js
+        }
+      }
+    ]
   })
 }
 
